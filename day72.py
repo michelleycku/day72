ccaabc4f25a146ffb4ec7f6d52f1058fc1775555
unknown <michelle.yc.ku@gmail.com>
2023-09-11 13:24:35 -0400

    Complete day72 challenge to day72.py

--- day72.py
+++ day72.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 df = pd.read_csv('QueryResults.csv', names=['DATE', 'TAG', 'POSTS'], header=0)
 # print(df)
@@ -29,9 +30,27 @@ reshaped_df.fillna(0, inplace=True)
 print(reshaped_df.isna().values.any())
 
 # print(reshaped_df)
+# print(reshaped_df.shape())
 # print(reshaped_df.columns)
 # print(reshaped_df.head())
 # print(reshaped_df.tail())
 # print(reshaped_df.count())
 
+# The window is number of observations that are averaged
+roll_df = reshaped_df.rolling(window=6).mean()
 
+plt.figure(figsize=(16,10))
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.xlabel('Date', fontsize=14)
+plt.ylabel('Number of Posts', fontsize=14)
+plt.ylim(0, 35000)
+
+# plot the roll_df instead
+for column in roll_df.columns:
+    plt.plot(roll_df.index, roll_df[column],
+             linewidth=3, label=roll_df[column].name)
+
+plt.legend(fontsize=16)
+
+plt.show()
